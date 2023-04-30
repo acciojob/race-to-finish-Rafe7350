83b36b21bd18851a0fccd4b27cf5c003e64dd8eb
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-04-30 13:06:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 window.promises = [];
-
 // Do not change the code above this
+
 // add your promises to the array `promises`
-window.promises = [];
+let x = Math.floor(Math.random() * 5) + 1;
+console.log(x);
+let promise1 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("1");
+	}, x * 1000)
+});
 
-// Add a new promise to the promises array
-window.promises.push(new Promise((resolve, reject) => {
-  // Generate a random time between 1 and 5 seconds
-  const randomTime = Math.floor(Math.random() * 5) + 1;
-  
-  // Simulate some asynchronous work
-  setTimeout(() => {
-    // Resolve the promise with the random time
-    resolve(randomTime);
-  }, randomTime * 1000);
-}));
+let promise2 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("2");
+	}, x * 1000)
+})
+	let promise3 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("3");
+	}, x * 1000)
+})
+	let promise4 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("4");
+	}, x * 1000)
+})
+	let promise5 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("5");
+	}, x * 1000)
+})
 
-// Do not change the code below this
-console.log(window.promises);
+	promises.push(promise1);
+	promises.push(promise2);
+	promises.push(promise3);
+	promises.push(promise4);
+	promises.push(promise5);
+
+	Promise.any(promises).then((value) => {
+    document.getElementById("output").innerText = value;
+  });
